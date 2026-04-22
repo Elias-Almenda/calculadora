6c1edab2d383f639ee2e8934100a312af955984e
Elias-Almenda <eliasalmenda64@gmail.com>
2026-04-22 18:50:41 -0300

    começo da adição de juros(ainda em fase de teste)

--- calculadora.py
+++ calculadora.py
@@ -4,11 +4,11 @@ print("Bem-vindo(a) Calculadora")
 
 while True:
   opcoes_validas = ['1','2','3','4','5','6','7']
-  print("\n[1] somar\n[2] multiplicar\n[3] subtrair\n[4] divisão\n[5] potencia \n[6] raiz\n[7] sair")
+  print("\n[1] somar\n[2] multiplicar\n[3] subtrair\n[4] divisão\n[5] potencia \n[6] raiz\n[7] juros\n[8] sair")
   
   choice = input("Escolha: ")
   
-  if choice == '7':
+  if choice == '8':
         print("Saindo.")
         break
 
@@ -21,6 +21,9 @@ while True:
   if choice not in opcoes_validas:
     print("Opção inválida! Tente novamente.")
     continue
+
+  if choice == '7':
+    n3 = float(input('digite o tempo do seu juros: '))
 
   n1 = float(input("Digite o primeiro número: "))
   n2 = float(input("Digite o segundo número: "))  
@@ -36,4 +39,6 @@ while True:
   elif choice == '5':
       print(f'Resultado da potencia/raiz de {n1} e {n2} é: ',modulo_matematico.potencial(n1,n2))
   elif choice == '6':
-      print(f'Resultado da raiz de {n1} e {n2} é: ',modulo_matematico.raiz(n1,n2))
+      print(f'Resultado da raiz de {n1} e {n2} é:', modulo_matematico.raiz(n1,n2))
+  elif choice == '7':
+      print(f'Resultado do juros simples é:', modulo_matematico.juros_simples(n1,n2,n3))
